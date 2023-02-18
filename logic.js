5af1f49f19ed0a7348a10778c5298ee6ccd587a5
wasih790 <120667351+wasih790@users.noreply.github.com>
2023-02-18 11:47:51 -0500

    Added a container div to the body

---
+++ logic.js
@@ -0,0 +1,6 @@
+let body = document.body
+// adding a container div to the body 
+const container = document.createElement('div');
+container.classList.add('container');
+
+body.appendChild(container)
