e855375080aa980130d5c9cc8067c17c57797f10
wasih790 <120667351+wasih790@users.noreply.github.com>
2023-02-18 12:13:27 -0500

    Created the first box
    Created a div for the box and added css as well, and added css for the
    container and column.

--- logic.js
+++ logic.js
@@ -3,10 +3,13 @@ let body = document.body
 const container = document.createElement('div');
 container.classList.add('container');
 
-body.appendChild(container)
+body.appendChild(container);
 
 //creating a column to hold a box/boxes
 const column = document.createElement('div');
-column.classList.add('column')
+column.classList.add('column');
 container.appendChild(column);
 
+const box = document.createElement('div')
+box.classList.add('box');
+column.appendChild(box)
